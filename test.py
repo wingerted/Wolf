8ad047778ce0e938a041e8a016921947f950a2d3
wingerted <wingerted@gmail.com>
2018-02-04 02:46:42 +0800

    Add Python Flatbuffer Test Example

---
+++ test.py
@@ -0,0 +1,18 @@
+import WolfEvent as wf
+import flatbuffers
+import mmap
+
+mmap_file = open("/Users/wingerted/test_shared_memory_dat", "rb")
+
+mmap_obj = mmap.mmap(mmap_file.fileno(), 0, prot=mmap.PROT_READ)
+
+#x = memoryview(mmap_obj)
+x = buffer(mmap_obj)
+b = bytearray(x)
+
+wolf = wf.WolfEvent.GetRootAsWolfEvent(x, 8)
+
+import time
+while True:
+    print(wolf.Id())
+    time.sleep(1)
