e63a3d97bb5fa97a2c5bc4e7ba6f18049abb2da2
wingerted <wingerted@gmail.com>
2018-02-21 02:26:34 +0800

    fix typo

--- lockfree_skiplist.h
+++ lockfree_skiplist.h
@@ -175,7 +175,7 @@ auto LockFreeSkipList<Key, Comparator>::FindLessThan(Key key) -> Node* {
                 if (curr == nullptr) {
                     break;
                 }
-                if (this->cmp_(curr->key, key) < 0) {
+                if (this->compare_(curr->key, key) < 0) {
                     pred = curr;
                     curr = this->GetNodeByOffset(curr->GetRef(i)->load().next_offset);
                 } else {
